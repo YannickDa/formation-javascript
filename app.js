31aae50265a3275237fdd55cead1a34cdb95deab
Yannick Dalbin <yannick@idvive.com>
2017-06-11 10:18:54 +0200

    TP1.4: Écoutez l'événement de changement sur le menu select puis exécutez la fonction créée précédemment

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const calcul1 = document.getElementsByName("calcul1")[0]
 const calcul2 = document.getElementsByName("calcul2")[0]
+const operator = document.getElementsByName("operator")[0]
 
 const updateValues = function () {
   console.log("Ma fonction est appelée")
@@ -7,3 +8,4 @@ const updateValues = function () {
 
 calcul1.addEventListener("keyup", updateValues)
 calcul2.addEventListener("keyup", updateValues)
+operator.addEventListener("change", updateValues)
