c572777f18c706ec7a0d56b2a344afc49e8ebc13
Yannick Dalbin <yannick@idvive.com>
2017-06-11 11:07:58 +0200

    TP2.3: Ajoutez un écouteur sur le submit du formulaire afin d'exécuter une fonction qui va permettre de traiter les informations saisies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const calcul1 = document.getElementsByName("calcul1")[0]
 const calcul2 = document.getElementsByName("calcul2")[0]
 const operator = document.getElementsByName("operator")[0]
+const formInscription = document.getElementsByName("inscription")[0]
 
 const updateValues = function () {
   const calcul1Value = parseInt(calcul1.value)
@@ -34,3 +35,10 @@ const updateValues = function () {
 calcul1.addEventListener("keyup", updateValues)
 calcul2.addEventListener("keyup", updateValues)
 operator.addEventListener("change", updateValues)
+
+const processForm = function (e) {
+  e.preventDefault()
+  console.log("Formulaire envoyé")
+}
+
+formInscription.addEventListener("submit", processForm)
