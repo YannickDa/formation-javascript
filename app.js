df131663b8d2eddd03079934df792653a1a2e9a2
Yannick Dalbin <yannick@idvive.com>
2017-06-11 10:24:20 +0200

    TP1.5: Dans la fonction, récupérez toutes les valeurs des différents champs puis effectuez le calcul

--- app.js
+++ app.js
@@ -3,7 +3,32 @@ const calcul2 = document.getElementsByName("calcul2")[0]
 const operator = document.getElementsByName("operator")[0]
 
 const updateValues = function () {
-  console.log("Ma fonction est appelée")
+  const calcul1Value = parseInt(calcul1.value)
+  const calcul2Value = parseInt(calcul2.value)
+  const operatorValue = operator.value
+
+  if (calcul1Value && calcul2Value) {
+    let result
+    switch (operatorValue) {
+      case "+":
+        result = calcul1Value + calcul2Value
+        break
+      case "-":
+        result = calcul1Value - calcul2Value
+        break
+      case "*":
+        result = calcul1Value * calcul2Value
+        break
+      case "/":
+        result = calcul1Value / calcul2Value
+        break
+      case "%":
+        result = calcul1Value % calcul2Value
+        break
+    }
+
+    console.log(result)
+  }
 }
 
 calcul1.addEventListener("keyup", updateValues)
